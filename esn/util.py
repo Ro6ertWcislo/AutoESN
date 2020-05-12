6745ec53d0dcd1215d198d4033b0ebff0f60f183
Ro6ertWcislo <robert.wcislo1995@gmail.com>
2020-05-12 17:47:44 +0200

    automatically choosing number of layers based on fft -version1

--- esn/util.py
+++ esn/util.py
@@ -1,9 +1,12 @@
 from copy import deepcopy
+from typing import Tuple, List
 
 from torch import Tensor
+import torch
 
-from esn.esn import DeepSubreservoirESN
-from utils.types import Metric
+from esn.esn import DeepSubreservoirESN, DeepESNCell, ESNCell
+from utils import math
+from utils.types import Metric, SpectralCentroid, SpectralSpread
 
 
 def fit_transform_DSESN(esn: DeepSubreservoirESN, input: Tensor, target: Tensor, test_input: Tensor,
@@ -19,7 +22,8 @@ def fit_transform_DSESN(esn: DeepSubreservoirESN, input: Tensor, target: Tensor,
 
     while new_result < best_result:
         if verbose > 0:
-            print(f'growing improved {getattr(metric,"__name__",type(metric).__name__)} from {best_result} to {new_result}')  # todo add logging
+            print(
+                f'growing improved {getattr(metric, "__name__", type(metric).__name__)} from {best_result} to {new_result}')  # todo add logging
         best_result = new_result
         best_model = deepcopy(esn)
         best_output = output
@@ -31,3 +35,59 @@ def fit_transform_DSESN(esn: DeepSubreservoirESN, input: Tensor, target: Tensor,
         new_result = metric(output.unsqueeze(-1), test_target).item()
 
     return best_model, best_output
+
+
+def FFT(mapped_states: Tensor) -> Tuple[Tensor, Tensor]:
+    timesteps = mapped_states.size(1)
+    frequencies = torch.zeros(mapped_states.size(0), timesteps // 2)
+    for i in range(mapped_states.size(0)):
+        signal = mapped_states[i]
+        signal_with_complex = torch.cat([signal.unsqueeze(-1), torch.zeros(timesteps, 1)], axis=1)
+        comps = torch.fft(signal_with_complex, signal_ndim=1)
+        frequencies[i, :] = torch.norm(comps[:timesteps // 2], p=2, dim=1)
+    avg_magnitudes = torch.mean(frequencies, dim=0)
+    return avg_magnitudes, torch.linspace(1, timesteps // 2, timesteps // 2) / timesteps
+
+
+def _spectral_centroid(magnitudes: Tensor, frequencies: Tensor) -> SpectralCentroid:
+    return (torch.sum(magnitudes * frequencies) / torch.sum(magnitudes)).item()
+
+
+def _spectral_spread(magnitudes: Tensor, frequencies: Tensor, centroid: float) -> SpectralSpread:
+    return (torch.sqrt(torch.sum(magnitudes * ((frequencies - centroid) ** 2)) / torch.sum(magnitudes))).item()
+
+
+def _spectral_distance_significant(centroid1: float, centroid2: float, spread1: float, tolerance: float) -> bool:
+    return abs(centroid1 - centroid2) > spread1 * tolerance
+
+
+def compute_spectral_statistics(mapped_states: Tensor) -> Tuple[SpectralCentroid, SpectralSpread]:
+    magnitudes, frequencies = FFT(mapped_states)
+    centroid = _spectral_centroid(magnitudes, frequencies)
+    spread = _spectral_spread(magnitudes, frequencies, centroid)
+
+    return centroid, spread
+
+
+def assign_layers(desn: DeepESNCell, input: Tensor, max_layers: int = 10, transient: int = 30,
+                  tolerance: float = 0.01) -> List[SpectralCentroid]:
+    in_training = True
+    desn.washout(input[:transient])
+    mapped_states = desn(input[transient:])
+    actual_centroid, actual_spread = compute_spectral_statistics(mapped_states)
+    centroids = [actual_centroid]
+
+    # todo to jest wersja globalna, tzn jak dodanie nowej warstwy wpłynie na całą odpowiedz sieci. Ma to sens ze bedzie sie stabilizowac
+    while in_training and len(desn.layers) <= max_layers:
+        next_layer = ESNCell(desn.hidden_size, desn.hidden_size, desn.bias, desn.initializer, desn.activation)
+
+        new_mapped_states = torch.cat([mapped_states, next_layer(mapped_states[:, -desn.hidden_size:])], axis=1)
+        new_centroid, new_spread = compute_spectral_statistics(new_mapped_states)
+        if _spectral_distance_significant(actual_centroid, new_centroid, actual_spread, tolerance):
+            desn.layers.append(next_layer)
+            desn.reset_hidden()
+            mapped_states, actual_centroid, actual_spread = new_mapped_states, new_centroid, new_spread
+        else:
+            in_training = False
+        centroids.append(new_centroid) #add also the one that is not in the final net
+    return centroids
--- utils/types.py
+++ utils/types.py
@@ -5,5 +5,7 @@ from torch import Tensor as Tensor
 Initializer = Callable[[Tensor, Optional[Tensor]], Tensor]
 Metric = Callable[[Tensor, Tensor], Tensor]
 ActivationFunction = Callable[[Tensor], Tensor]
+SpectralCentroid =float
+SpectralSpread =float
 
 # subreservoiry ogarnac inicjalizacja -mozna podac wczesniejszy sub
